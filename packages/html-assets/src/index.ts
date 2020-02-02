893e3b956f8f885ebff78f378dd664dac3042ba6
Matt Gaunt <mattgaunt@google.com>
2020-02-02 11:17:48 -0800

    Removing logs

--- packages/html-assets/src/index.ts
+++ packages/html-assets/src/index.ts
@@ -69,9 +69,6 @@ export async function processFiles(opts: Options) {
 
   for (const h of htmlFiles) {
     const assets = await getAssetsForHTMLFile(h, opts.assetPaths, opts.extensions);
-    console.log(`File: ${h}`);
-    console.log('Assets:', assets);
-    console.log('');
     const newContents = await injectAssets(assets);
     await fs.writeFile(h, newContents);
   }
